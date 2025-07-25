9860b143e9cb51313c60aed0651beac695d30ac7
Ivana Ivanovic <ivana.ivanovic.94@hotmail.com>
2025-07-25 03:19:13 +0200

    Nav bar

---
+++ src/app/members/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MembersPage() {
+  return (
+    <div>
+      <h3 className="text-3xl">Members Page</h3>
+      <Link href="/"> Go back to Home</Link>
+    </div>
+  )
+}
---
+++ src/components/navbar/NavLink.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { NavbarItem } from "@heroui/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Props = {
+  href: string;
+  label: string;
+}
+
+export default function NavLink({href, label}: Props) {
+  const pathName = usePathname();
+  return (
+    <NavbarItem isActive={pathName === href} as={Link} href={href}>{label}</NavbarItem>
+  )
+}
